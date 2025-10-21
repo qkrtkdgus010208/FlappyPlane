237fa562f37366893239d1dad4de20bf5b0ace20
qkrtkdgus010208 <psh010208@naver.com>
2025-10-21 20:38:51 +0900

    feat: 1-5. 캐릭터 움직임과 점프 로직 구현

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,90 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Animator animator = null;
+    private Rigidbody2D rigid = null;
+
+    private float flapForce = 6f;
+    private float forwardSpeed = 3f;
+    private bool isDead = false;
+    private float deathCooldown = 0f;
+
+    private bool isFlap = false;
+
+    private bool godMode = false;
+
+    void Start()
+    {
+        animator = transform.GetComponentInChildren<Animator>();
+        rigid = transform.GetComponent<Rigidbody2D>();
+
+        if (animator == null)
+        {
+            Debug.LogError("Not Founded Animator");
+        }
+
+        if (rigid == null)
+        {
+            Debug.LogError("Not Founded Rigidbody");
+        }
+    }
+
+    void Update()
+    {
+        if (isDead)
+        {
+            if (deathCooldown <= 0)
+            {
+                if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
+                {
+                    // 게임 재시작
+                }
+            }
+            else
+            {
+                deathCooldown -= Time.deltaTime;
+            }
+        }
+        else
+        {
+            if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
+            {
+                isFlap = true;
+            }
+        }
+    }
+
+    public void FixedUpdate()
+    {
+        if (isDead)
+            return;
+
+        Vector3 velocity = rigid.velocity;
+        velocity.x = forwardSpeed;
+
+        if (isFlap)
+        {
+            velocity.y += flapForce;
+            isFlap = false;
+        }
+
+        rigid.velocity = velocity;
+
+        float angle = Mathf.Clamp((rigid.velocity.y * 10f), -90, 90);
+        transform.rotation = Quaternion.Euler(0, 0, angle);
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (godMode)
+            return;
+
+        if (isDead)
+            return;
+
+        animator.SetInteger("IsDie", 1);
+        isDead = true;
+        deathCooldown = 1f;
+    }
+}
