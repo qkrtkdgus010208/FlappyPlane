6d632d70654014a07454a99bfee975b9cba7e032
qkrtkdgus010208 <psh010208@naver.com>
2025-10-22 11:41:15 +0900

    feat: 1-8. 게임 오버와 점수 UI 구현

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -10,16 +10,30 @@ public class GameManager : MonoBehaviour
         get { return gameManager; }
     }
 
+    UIManager uiManager;
+
+    public UIManager UIManager
+    {
+        get { return uiManager; }
+    }
+
     private int currentScore = 0;
 
     private void Awake()
     {
         gameManager = this;
+        uiManager = FindObjectOfType<UIManager>();
+    }
+
+    private void Start()
+    {
+        uiManager.UpdateScore(0);
     }
 
     public void GameOver()
     {
         Debug.Log("Game Over");
+        uiManager.SetRestart();
     }
 
     public void RestartGame()
@@ -30,8 +44,7 @@ public class GameManager : MonoBehaviour
     public void AddScore(int score)
     {
         currentScore += score;
-
+        uiManager.UpdateScore(currentScore);
         Debug.Log("Score: " + currentScore);
     }
-
 }
